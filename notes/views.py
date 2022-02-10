fdc005dfd1bdd222485a288ccb6305695114142d
SHMF1385 <shmf.mousavi1385@gmail.com>
2022-02-10 13:11:48 +0330

    fix bugs in notes view

--- notes/views.py
+++ notes/views.py
@@ -12,7 +12,7 @@ class getNoteView(View):
 
     def get(self, request):
         return JsonResponse({"Status": "ERR_REQUEST_TYPE_IS_GET"})
-    
+
     def post(self, request):
         username = request.POST.get('username')
         token = request.POST.get('token')
@@ -43,11 +43,11 @@ class getNoteView(View):
                 return JsonResponse({"Status": "NOTE_IS_NOTE_DEFIND"})
             return JsonResponse({"Status": "AUTHENTICATION_FAILED"})
         return JsonResponse({"Status": "ERR_ARGS"})
-                
+
 
 @method_decorator(csrf_exempt, name="dispatch")
 class getNotesView(View):
-    
+
     def get(self, request):
         return JsonResponse({"Status": "ERR_REQUEST_TYPE_IS_GET"})
 
@@ -57,7 +57,7 @@ class getNotesView(View):
         if username and token:
             user = User.objects.filter(username=username, token__token=token)
             if user:
-                notes = Note.objects.filter(user=user[0])
+                notes = Note.objects.filter(user=user[0])[::-1]
                 context = {}
                 for note in notes:
                     context[note.title] = {'content': note.content, 'pk': note.pk}
@@ -78,16 +78,17 @@ class newNoteView(View):
         noteType = request.POST.get('noteType')
         content = request.POST.get('content')
         if username and token and noteType and content:
-            user = get_object_or_404(User, token__token=token)
+            user = User.objects.filter(username=username, token=token)
             if user:
+                user = uesr[0]
                 if not Note.objects.filter(title=title):
                     if noteType in Note.allowed_types:
-                        Note.objects.create(
+                        note = Note.objects.create(
                             title = title,
                             user = user,
                             content = content,
                         )
-                        return JsonResponse({"Status": "FILE_CREATED"})
+                        return JsonResponse({"Status": "FILE_CREATED", "pk": note.pk})
                     return JsonResponse({"Status": "ERR_TYPE_IS_INCORRECT"})
                 return JsonResponse({"Status": "ERR_TITLE_EXISTS"})
             return JsonResponse({"Status": "AUTHENTICATION_FAILED"})
@@ -106,18 +107,21 @@ class editNoteView(View):
         pk = request.POST.get('pk')
         content = request.POST.get('content')
         title = request.POST.get('title')
-        
+
         if username and token and pk and content and title:
-            user = get_object_or_404(User, token__token = token)
+            user = User.objects.filter(username=username, token=token)
             if user:
-                note = get_object_or_404(Note, pk = pk, user=user)
+                note = Note.objects.filter(user=user, pk=pk)
                 if note:
+                    note = note[0]
                     note.title = title
                     note.content = content
                     note.save()
                     return JsonResponse({"Status": "NOTE_EDITED"})
+                    return JsonResponse({"Status": "AUTHENTICATION_FAILED"})
+                return JsonResponse({"Status": "NOTE_NOT_EXISTS"})
         return JsonResponse({"Status": "ERR_ARGS"})
-                    
+
 
 @method_decorator(csrf_exempt, name="dispatch")
 class deleteNoteView(View):
@@ -130,14 +134,16 @@ class deleteNoteView(View):
         token = request.POST.get('token')
         pk = request.POST.get('pk')
         if username and token and pk:
-            user = get_object_or_404(User, token__token=token)
+            user = User.objects.filter(username=username, token=token)
             if user:
-                note = get_object_or_404(
-                    Note,
-                    pk = pk,
-                    user = user
+                user = user[0]
+                note = Note.objects.filter(
+                    user = user,
+                    pk = pk
                 )
                 if note:
                     note.delete()
                     return JsonResponse({"Status": "NOTE_DELETED"})
+                return JsonResponse({"Status": "NOTE_NOT_EXISTS"})
+            return JsonResponse({"Status": "AUTHENTICATION_FAILED"})
         return JsonResponse({"Status": "ERR_ARGS"})
