2872410c33cc5c03680659e00438a4c7a50c8fe4
SHMF1385 <shmf.mousavi1385@gmail.com>
2022-02-24 14:02:59 +0330

    add model notebook

--- notes/admin.py
+++ notes/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Note
+from .models import NoteBook, Note
 
 # Register your models here.
 
+admin.site.register(NoteBook)
 admin.site.register(Note)
--- notes/models.py
+++ notes/models.py
@@ -1,12 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class NoteBook(models.Model):
+    title = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user}/{self.title}"
+
 class Note(models.Model):
     title = models.CharField(max_length=24)
+    notebook = models.ForeignKey(NoteBook, on_delete=models.CASCADE, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     noteType = models.CharField(max_length=24)
     datetime = models.DateTimeField(auto_now=True)
     content = models.TextField(blank=True)
     allowed_types = ['note', 'kanban', 'table']
     def __str__(self):
-        return f"{self.user}/{self.title}"
+        return f"{self.user}/{self.notebook.title}/{self.title}"
